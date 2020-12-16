5d345b0d7e688e517e276e90b124d238ce53b25a
mak <maxnull@hotmail.com>
2020-12-16 14:20:40 +0800

    add trap.cpp

--- trap.cpp
+++ trap.cpp
@@ -6,8 +6,8 @@ class test
 {
 private:
 public:
-    static const int attr = 0;
-
+    static const int attr1 = 0;
+    static int attr2;
     test(/* args */){}
     ~test(){}
     void func(const int & a)
@@ -16,10 +16,11 @@ public:
     }
 };
 
+int test::attr2 = 1;
+
 int main(int, char**) 
 {
     test t;
-    t.func(1);
-    t.func(test::attr);
-    t.func(t.attr);
+    t.func(test::attr1);
+    t.func(test::attr2);
 }
